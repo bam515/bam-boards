2f9691f0f938e6acf03e4edd1db2a97715eafdba
bam <devbam515@gmail.com>
2023-03-14 19:54:49 +0900

    model and migration

---
+++ database/migrations/2023_03_14_080522_create_boards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->id('boardID'); // 해당 테이블의 Pirmary Key를 bigIncrements(auto-incrementing big integer) 타입으로 생성
+            $table->string('boardTitle', 255); // boardTitle라는 이름의 컬럼을 varchar(255) 타입으로 생성
+            $table->longText('boardContent'); // boardContent라는 이름의 컬럼을 longtext 타입으로 생성
+            $table->timestamp('createdAt'); // createdAt라는 이름의 컬럼을 timestamp 타입으로 생성
+            $table->timestamp('updatedAt'); // updatedAt라는 이름의 컬럼을 timestamp 타입으로 생성
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
