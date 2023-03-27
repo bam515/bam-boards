991cbde0f84b67abcba58832e54411c9716d165a
bam <devbam515@gmail.com>
2023-03-27 13:53:26 +0900

    swagger api

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -23,6 +23,27 @@ class BoardController extends Controller
         return view('boards.create');
     }
 
+    /**
+     * @OA\Post (
+     *     path="/boards",
+     *     tags={"게시판"},
+     *     summary="게시글 등록",
+     *     description="게시글을 등록",
+     *     @OA\RequestBody(
+     *         description="게시글 정보",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema (
+     *                 @OA\Property (property="board_title", type="string", description="게시글 제목", example="공지사항입니다."),
+     *                 @OA\Property (property="board_content", type="string", description="게시글 내용", example="공지사항 내용입니다.")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Success"),
+     *     @OA\Response(response="400", description="Fail")
+     * )
+     */
     public function store(Request $request) {
 
         $request = $request->validate([
@@ -47,6 +68,28 @@ class BoardController extends Controller
         return view('boards.edit', compact('board'));
     }
 
+    /**
+     * @OA\Put (
+     *     path="/boards/{board_id}",
+     *     tags={"게시판"},
+     *     summary="게시글 수정",
+     *     description="게시글을 수정",
+     *     @OA\Parameter (name="board_id", in="path", description="게시글 PK"),
+     *     @OA\RequestBody(
+     *         description="게시글 정보",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema (
+     *                 @OA\Property (property="board_title", type="string", description="게시글 제목", example="공지사항입니다."),
+     *                 @OA\Property (property="board_content", type="string", description="게시글 내용", example="공지사항 내용입니다.")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Success"),
+     *     @OA\Response(response="400", description="Fail")
+     * )
+     */
     public function update(Board $board, Request $request) {
         $request = $request->validate([
             'board_title' => 'required',
@@ -60,6 +103,17 @@ class BoardController extends Controller
         return redirect()->route('board.index');
     }
 
+    /**
+     * @OA\Delete (
+     *     path="/boards/{board_id}",
+     *     tags={"게시판"},
+     *     summary="게시글 삭제",
+     *     description="게시글을 삭제",
+     *     @OA\Parameter (name="board_id", in="path", description="게시글 PK"),
+     *     @OA\Response(response="200", description="Success"),
+     *     @OA\Response(response="400", description="Fail")
+     * )
+     */
     public function destroy(Board $board) {
         $board->delete();
         return redirect()->route('board.index');
