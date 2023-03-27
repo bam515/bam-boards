ec9d5718e3dd624688d7abbf3d7a470a486ed57c
bam515 <devbam515@gmail.com>
2023-03-27 22:49:28 +0900

    update view

--- resources/views/boards/index.blade.php
+++ resources/views/boards/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <h2 class="mt-4 mb-3">라라벨 게시판</h2>
 
-    <a href="{{ route('board.create') }}"><button type="button" class="btn btn-dark" style="float: right;">글 작성</button></a>
+    <a href="{{ route('board.create') }}">
+        <button type="button" class="btn btn-dark" style="float: right;">글 작성</button>
+    </a>
 
     <table class="table table-striped table-hover">
         <colgroup>
@@ -21,21 +23,31 @@
         </tr>
         </thead>
         <tbody class="table-group-divider">
+        {{-- 게시글의 번호를 내림차순으로 표시해주기 위해 작성 --}}
         @php
             $no = $boards->total() - (($boards->currentPage() - 1) * $boards->perPage());
         @endphp
+        {{-- 반복문 --}}
         @foreach($boards as $key => $board)
             <tr>
                 <th scope="row" class="align-middle">{{ $no-- }}</th>
-                <td class="align-middle"><a href="{{ route('board.show', $board) }}">{{ $board->board_title }}</a></td>
+                <td class="align-middle">
+                    <a href="{{ route('board.show', ['board' => $board->board_id]) }}">{{ $board->board_title }}</a>
+                </td>
                 <td class="align-middle">{{ $board->created_at }}</td>
                 <td class="align-middle">
-                    <button type="button" class="btn btn-dark" onclick="location.href='{{ route('board.edit', $board) }}'">수정</button>
-                    <form action="{{ route('board.delete', $board->board_id) }}" method="post"></form>
-                    <button type="button" class="btn btn-danger" onclick="location.href='{{ route('board.delete', $board) }}'">삭제</button>
+                    <a href="{{ route('board.edit', ['board' => $board->board_id]) }}">
+                        <button class="btn btn-dark">수정</button>
+                    </a>
+                    <a href="{{ route('board.delete', ['board' => $board->board_id]) }}">
+                        <button class="btn btn-danger">삭제</button>
+                    </a>
                 </td>
             </tr>
         @endforeach
         </tbody>
     </table>
+
+    {{-- 페이지네이션 --}}
+    {{ $boards->withQueryString()->links() }}
 @endsection
